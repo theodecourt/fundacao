5a41a22d9cecd4359363006b1fa353dce0a2f606
theo <decourtheo@gmail.com>
2024-10-03 10:10:45 -0300

    curriculo Luciano Decourt

--- pc.py
+++ pc.py
@@ -97,13 +97,43 @@ def main():
     st.title('Luciano Decourt')  # Adiciona o título "Luciano Decourt"
     
     # Adiciona os quatro botões no topo
-    # st.write("### Menu")
     col1, col2, col3, col4 = st.columns(4)
     
     with col1:
         if st.button('O engenheiro'):
-            st.write("Botão 'O engenheiro' pressionado.")
+            st.write("**Luciano Décourt**")
+            st.write("""
+            **Formação Profissional:**
+            - Escola Politécnica da Universidade de São Paulo, POLI, USP (1963)
+            - Harvard University (1965)
+            - Cambridge University (1972)
             
+            **Empresas onde atuou e/ou atua:**
+            - Instituto de Pesquisas Técnológicas (IPT), Seção de Solos
+            - Brasconsult, Engenharia de Projetos
+            - Laboratório Rankine de Engenharia Civil
+            - Luciano Décourt Consultoria
+            - Faculdade de Engenharia da FAAP (Professor Titular de Mecânica dos Solos)
+            
+            **Entidades e Premiações:**
+            - Vice-presidente da International Society for Soil Mechanics and Foundation Engineering (1989 – 1994)
+            - Sócio Emérito da Associação Brasileira de Mecânica dos Solos e Engenharia Geotécnica (ABMS)
+            - Fellow da American Society of Civil Engineers (ASCE)
+            - Membro da Academia Nacional de Engenharia (ANE)
+            
+            **Prêmios:**
+            - Manuel Rocha, Karl Terzaghi, José Machado (ABMS)
+            - Odair Grillo (ABEF)
+            - Milton Vargas (Revista Fundações e Obras Geotécnicas)
+            
+            **Contribuições:**
+            - Mais de 150 trabalhos publicados
+            
+            **Destaques:**
+            - Desenvolvimento do Standard Penetration Test (SPT), International Reference Test Procedure (1988/1989)
+            - Estudos pioneiros sobre solos lateríticos nos últimos 30 anos
+            """)
+    
     with col2:
         if st.button('Obras'):
             st.write("Botão 'Obras' pressionado.")
