d075f5e9fc462cb74c347214a34829d0378c4bcf
theo <decourtheo@gmail.com>
2024-10-03 10:15:00 -0300

    div pags

--- pc.py
+++ pc.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import math
 from scipy.optimize import fsolve
 
+# Importa o currículo do arquivo curriculo.py
+from curriculo import mostrar_curriculo
+
 # Função para carregar a tabela
 def carregar_tabela():
     uploaded_file = st.file_uploader("Escolha o arquivo CSV", type="csv")
@@ -101,38 +104,7 @@ def main():
     
     with col1:
         if st.button('O engenheiro'):
-            st.write("**Luciano Décourt**")
-            st.write("""
-            **Formação Profissional:**
-            - Escola Politécnica da Universidade de São Paulo, POLI, USP (1963)
-            - Harvard University (1965)
-            - Cambridge University (1972)
-            
-            **Empresas onde atuou e/ou atua:**
-            - Instituto de Pesquisas Técnológicas (IPT), Seção de Solos
-            - Brasconsult, Engenharia de Projetos
-            - Laboratório Rankine de Engenharia Civil
-            - Luciano Décourt Consultoria
-            - Faculdade de Engenharia da FAAP (Professor Titular de Mecânica dos Solos)
-            
-            **Entidades e Premiações:**
-            - Vice-presidente da International Society for Soil Mechanics and Foundation Engineering (1989 – 1994)
-            - Sócio Emérito da Associação Brasileira de Mecânica dos Solos e Engenharia Geotécnica (ABMS)
-            - Fellow da American Society of Civil Engineers (ASCE)
-            - Membro da Academia Nacional de Engenharia (ANE)
-            
-            **Prêmios:**
-            - Manuel Rocha, Karl Terzaghi, José Machado (ABMS)
-            - Odair Grillo (ABEF)
-            - Milton Vargas (Revista Fundações e Obras Geotécnicas)
-            
-            **Contribuições:**
-            - Mais de 150 trabalhos publicados
-            
-            **Destaques:**
-            - Desenvolvimento do Standard Penetration Test (SPT), International Reference Test Procedure (1988/1989)
-            - Estudos pioneiros sobre solos lateríticos nos últimos 30 anos
-            """)
+            st.write(mostrar_curriculo())
     
     with col2:
         if st.button('Obras'):
