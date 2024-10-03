ffc10af89de11f9d4689d306dc9e10135c1940f6
theo <decourtheo@gmail.com>
2024-10-03 15:17:54 -0300

    menu lateral8

--- pc.py
+++ pc.py
@@ -5,7 +5,6 @@ from programas import pagina_programas
 # Função principal para a página
 def pagina_principal():
     st.title('Luciano Decourt')
-    st.write("Bem-vindo ao site de Luciano Decourt")
     st.image("foto.jpeg", caption="Luciano Decourt", width=300)
 
 def pagina_engenheiro():
@@ -24,10 +23,11 @@ def pagina_artigos():
 def main():
     st.sidebar.title("Navegação")
 
+    st.sidebar.title("Selecione a página:")
+
     # Exibe as opções de navegação como Radio Button
     pagina = st.sidebar.radio(
-        "Selecione a página:",
-        ["Página Principal", "O engenheiro", "Obras", "Artigos", "Programas"]
+        ["Página Principal", "O Engenheiro", "Obras", "Artigos", "Programas"]
     )
 
     # Exibe a página correspondente
