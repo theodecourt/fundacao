43baed653df311d8bc9c8968d752ee641f303c40
theo <decourtheo@gmail.com>
2024-10-03 15:12:19 -0300

    menu lateral5

--- pc.py
+++ pc.py
@@ -1,28 +1,21 @@
 import streamlit as st
-from curriculo import mostrar_curriculo  # Importa a função para exibir a foto
-from programas import pagina_programas  # Importa a função da página Programas
+from curriculo import mostrar_curriculo
+from programas import pagina_programas
 
 # Função principal para a página
 def pagina_principal():
     st.title('Luciano Decourt')
-    
     st.write("Bem-vindo ao site de Luciano Decourt")
-    
-    # Carregar e exibir a imagem na página principal
     st.image("foto.jpeg", caption="Luciano Decourt", width=300)
-    
 
-# Função para a página "O engenheiro"
 def pagina_engenheiro():
     st.title('Luciano Decourt')
     st.markdown(mostrar_curriculo(), unsafe_allow_html=True)
 
-# Função para a página "Obras"
 def pagina_obras():
     st.title('Obras de Luciano Decourt')
     st.write("Conteúdo de obras será adicionado aqui.")
 
-# Função para a página "Artigos"
 def pagina_artigos():
     st.title('Artigos de Luciano Decourt')
     st.write("Conteúdo de artigos será adicionado aqui.")
@@ -30,21 +23,22 @@ def pagina_artigos():
 # Função principal para a navegação
 def main():
     st.sidebar.title("Navegação")
-    pagina = st.sidebar.selectbox(
-        "Selecione a página:",
-        ["Página Principal", "O engenheiro", "Obras", "Artigos", "Programas"]
-    )
 
-    if pagina == "Página Principal":
-        pagina_principal()  # Chama a função da página principal
-    elif pagina == "O engenheiro":
+    # Exibe as páginas como botões no menu lateral
+    if st.sidebar.button("Página Principal"):
+        pagina_principal()
+    elif st.sidebar.button("O engenheiro"):
         pagina_engenheiro()
-    elif pagina == "Obras":
+    elif st.sidebar.button("Obras"):
         pagina_obras()
-    elif pagina == "Artigos":
+    elif st.sidebar.button("Artigos"):
         pagina_artigos()
-    elif pagina == "Programas":
-        pagina_programas()  # Chama a função de programas importada de programas.py
+    elif st.sidebar.button("Programas"):
+        pagina_programas()
+
+    # Exibe a página principal por padrão
+    if "pagina" not in st.session_state:
+        pagina_principal()
 
 if __name__ == '__main__':
     main()
