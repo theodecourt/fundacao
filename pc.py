7278549d59c395ce017b21ecc76bbf597ac19138
theo <decourtheo@gmail.com>
2024-10-03 15:10:01 -0300

    menu lateral4

--- pc.py
+++ pc.py
@@ -5,12 +5,8 @@ from programas import pagina_programas  # Importa a função da página Programa
 # Função principal para a página
 def pagina_principal():
     st.title('Luciano Decourt')
-    
     st.write("Bem-vindo ao site de Luciano Decourt")
-
-    # Carregar e exibir a imagem na página principal
     st.image("foto.jpeg", caption="Luciano Decourt", width=300)
-    
 
 # Função para a página "O engenheiro"
 def pagina_engenheiro():
@@ -27,23 +23,39 @@ def pagina_artigos():
     st.title('Artigos de Luciano Decourt')
     st.write("Conteúdo de artigos será adicionado aqui.")
 
+# Função principal para a navegação
 def main():
     st.sidebar.title("Navegação")
+    
+    # Definir um estado inicial para a página
+    pagina_selecionada = "Página Principal"
 
     # Exibe todas as opções como botões no menu lateral
     if st.sidebar.button("Página Principal"):
-        pagina_principal()
-
+        pagina_selecionada = "Página Principal"
+    
     if st.sidebar.button("O engenheiro"):
-        pagina_engenheiro()
-
+        pagina_selecionada = "O engenheiro"
+    
     if st.sidebar.button("Obras"):
-        pagina_obras()
-
+        pagina_selecionada = "Obras"
+    
     if st.sidebar.button("Artigos"):
-        pagina_artigos()
-
+        pagina_selecionada = "Artigos"
+    
     if st.sidebar.button("Programas"):
+        pagina_selecionada = "Programas"
+
+    # Exibe a página selecionada
+    if pagina_selecionada == "Página Principal":
+        pagina_principal()
+    elif pagina_selecionada == "O engenheiro":
+        pagina_engenheiro()
+    elif pagina_selecionada == "Obras":
+        pagina_obras()
+    elif pagina_selecionada == "Artigos":
+        pagina_artigos()
+    elif pagina_selecionada == "Programas":
         pagina_programas()
 
 if __name__ == '__main__':
