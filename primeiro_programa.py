5488cea645ce665b760b3c0f3eacac3f192c8a04
theodecourt <theod@al.insper.edu.br>
2024-10-13 11:25:38 -0300

    num romanos6

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -179,17 +179,17 @@ def calcular_regressao(tabela, num_regressoes, pontos_tipos, diametro_estaca, id
         plt.plot(x, y, colors[i], label=f'Regressão {i+1}')
         
         if idioma == "Português":
-            st.write(f'Pontos utilizados na regressão {num_romanos[i+1]}: {lin_in} até {lin_fim}')
+            st.write(f'Pontos utilizados na regressão {i+1}: {lin_in} até {lin_fim}')
             st.write('Tipo de regressão:', tipo_regressao.capitalize())
             st.write('Equação da regressão:', equacao)
             st.write('R²:', R_sq)
-            st.write(f'Quc para a regressão {num_romanos[i+1]}: {quc:.4f} tf')
+            st.write(f'Quc para a regressão {i+1}: {quc:.4f} tf')
         else:
-            st.write(f'Points used in regression {num_romanos[i+1]}: {lin_in} to {lin_fim}')
+            st.write(f'Points used in regression {i+1}: {lin_in} to {lin_fim}')
             st.write('Regression type:', tipo_regressao.capitalize())
             st.write('Regression equation:', equacao)
             st.write('R²:', R_sq)
-            st.write(f'Quc for regression {num_romanos[i+1]}: {quc:.4f} tf')
+            st.write(f'Quc for regression {i+1}: {quc:.4f} tf')
 
 
         # Adiciona a regressão e o tipo de regressão à lista
@@ -251,7 +251,7 @@ def primeiro_programa(idioma):
             for i in range(num_regressoes):
                 lin_in = st.number_input(f'Ponto inicial {i+1}:', min_value=1, max_value=len(tabela), value=1)
                 lin_fim = st.number_input(f'Ponto final {i+1}:', min_value=lin_in, max_value=len(tabela), value=len(tabela))
-                tipo_regressao = st.selectbox(f'Tipo de regressão {num_romanos[i+1]}:', ['linear', 'log'], index=0)
+                tipo_regressao = st.selectbox(f'Tipo de regressão {i+1}:', ['linear', 'log'], index=0)
                 pontos_tipos.append((lin_in, lin_fim, tipo_regressao))
             
             if st.button('Calcular Regressões'):
