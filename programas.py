e507482dbc5c8bf0e82320b61ce40f956adcdf9e
Theo dec <decourtheo@gmail.com>
2024-12-01 20:05:52 -0300

    lu5

--- programas.py
+++ programas.py
@@ -2,40 +2,8 @@ import streamlit as st
 from primeiro_programa import primeiro_programa  # Importa o primeiro programa
 from segundo_programa import segundo_programa  # Importa o segundo programa
 
-# Função para injetar CSS personalizado
-def set_background_and_text_color():
-    st.markdown(
-        """
-        <style>
-        /* Define o fundo da página */
-        body {
-            background-color: #000000; /* Preto */
-            color: #FFFFFF; /* Branco */
-        }
-
-        /* Define o fundo da barra lateral */
-        .css-1d391kg { /* Pode variar dependendo da versão do Streamlit */
-            background-color: #000000;
-        }
-
-        /* Define a cor do texto nos elementos do Streamlit */
-        .css-1v0mbdj {
-            color: #FFFFFF;
-        }
-
-        /* Ajusta outros elementos conforme necessário */
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-
-
 # Função para a página "Programas"
 def pagina_programas(idioma):
-
-    # Chama a função para aplicar o CSS
-    set_background_and_text_color() 
     if idioma == "Português":
         st.title('Programas de Luciano Décourt')
 
@@ -63,7 +31,3 @@ def pagina_programas(idioma):
         elif program_selected == 'ZDSM':
             segundo_programa(idioma)
 
-# Exemplo de como chamar a função pagina_programas
-# Defina o idioma conforme necessário, por exemplo:
-idioma_selecionado = st.sidebar.selectbox('Selecione o idioma:', ['Português', 'English'])
-pagina_programas(idioma_selecionado)
